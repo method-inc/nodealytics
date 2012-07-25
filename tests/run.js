3128730687b4c9ba3ae45cf8aab187f6b7efb27f
rockbot <raquel@skookum.com>
2012-07-25 09:38:46 -0400

    Starting up the googalytics module!

---
+++ tests/run.js
@@ -0,0 +1,8 @@
+var GA = require("googalytics");
+var should = require("should");
+var assert = require("assert");
+
+describe('just get started', function (done) {
+  (1).should.eql(1);
+  done();
+});
